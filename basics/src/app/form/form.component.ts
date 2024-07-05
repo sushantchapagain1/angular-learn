4fbc642980812e939594ff3fb6baea5ef90f5c78
sushantsursatech1 <sushant.chapagain@sursatech.com>
2024-07-05 11:49:12 +0545

    data binding and display basic task

--- basics/src/app/form/form.component.ts
+++ basics/src/app/form/form.component.ts
@@ -1,20 +1,28 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css'],
 })
-export class FormComponent {
-  userName: string;
+export class FormComponent implements AfterViewInit {
+  // userName: string;
+  // textValue: string = 'lorem ipsum dolor sit amet';
+  // handleKeyUp() {
+  //   console.log(this.userName);
+  // }
+  // onKeyUp() {
+  //   // when
+  //   console.log(this.textValue);
+  // }
 
-  textValue: string = 'lorem ipsum dolor sit amet';
+  title: string;
+  desc: string;
+  imageUrl: string;
+  openUrl: string;
+  addBackground: boolean;
 
-  handleKeyUp() {
-    console.log(this.userName);
-  }
-  onKeyUp() {
-    // when
-    console.log(this.textValue);
+  ngAfterViewInit(): void {
+    console.log(this.title, this.openUrl);
   }
 }
