204fd2fd818aa3c597285d0959a0e57b8e7b8be8
sushantsursatech1 <sushant.chapagain@sursatech.com>
2024-07-05 10:16:40 +0545

    event filtering

--- basics/src/app/post-list/post-list.component.ts
+++ basics/src/app/post-list/post-list.component.ts
@@ -13,4 +13,23 @@ type TPost = {
 })
 export class PostListComponent {
   @Input() post: TPost;
+  imageUrl: string =
+    'https://testsigma.com/blog/wp-content/uploads/Random-Testing-In-Software-Testing-An-Overview.png';
+
+  shouldBeTeal: boolean = true;
+  largeFontSize: string = '32px';
+
+  eventBindingAngular() {
+    console.log('Button clicked event.');
+  }
+
+  handleKeyUp(e: KeyboardEvent) {
+    if (e.keyCode === 13 && e.key === 'Enter') {
+      console.log('Enter pressed..');
+    }
+  }
+
+  handleKeyUpEventFiltering() {
+    console.log('Enter pressed received from event filtering of angular.');
+  }
 }
