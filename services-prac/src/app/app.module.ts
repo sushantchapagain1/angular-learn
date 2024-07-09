be79afb159a724506a8dfc82648e39870aa96a0e
sushantsursatech1 <sushant.chapagain@sursatech.com>
2024-07-09 09:48:47 +0545

    app module provider

--- services-prac/src/app/app.module.ts
+++ services-prac/src/app/app.module.ts
@@ -4,17 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostsComponent } from './posts/posts.component';
+import { PostService } from './services/post.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PostsComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, PostsComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  // proving the providers in the app module so that all components can use.
+  providers: [PostService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- services-prac/src/app/posts/posts.component.ts
+++ services-prac/src/app/posts/posts.component.ts
@@ -1,3 +1,4 @@
+// Not recmmended way just creating new instance.
 // import { Component, OnInit } from '@angular/core';
 
 // import { PostService } from '../services/post.service';
@@ -17,6 +18,31 @@
 //   ngOnInit(): void {}
 // }
 
+// import { Component, OnInit } from '@angular/core';
+
+// import { PostService } from '../services/post.service';
+
+// @Component({
+//   selector: 'app-posts',
+//   templateUrl: './posts.component.html',
+//   styleUrl: './posts.component.css',
+//   // after proving the service in the provider this component we can inject the service if not provided throws NullInjectorError.
+//   providers: [PostService],
+// })
+// export class PostsComponent implements OnInit {
+//   posts: Array<any>;
+
+//   // dependency injection. instead of creating a new instance of the service we accept the service in the constructor and assign.
+//   constructor(private postService: PostService) {
+//     this.posts = postService.postList;
+//   }
+//   ngOnInit(): void {}
+// }
+
+// creating the mutiple instance of a same service is a very bad practice
+
+// There are two solutions to one is proving the providers in the app module and using for all components.
+
 import { Component, OnInit } from '@angular/core';
 
 import { PostService } from '../services/post.service';
@@ -25,13 +51,10 @@ import { PostService } from '../services/post.service';
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrl: './posts.component.css',
-  // after proving the service in the provider this component we can inject the service if not provided throws NullInjectorError.
-  providers: [PostService],
 })
 export class PostsComponent implements OnInit {
   posts: Array<any>;
 
-  // dependency injection. instead of creating a new instance of the service we accept the service in the constructor and assign.
   constructor(private postService: PostService) {
     this.posts = postService.postList;
   }
