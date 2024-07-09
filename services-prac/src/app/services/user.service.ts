72fb571c6ebca8bdd6920b0ba1e51a2201bf6875
sushantsursatech1 <sushant.chapagain@sursatech.com>
2024-07-09 10:02:12 +0545

    gnerate service command

---
+++ services-prac/src/app/services/user.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+// ng g s services/user command to generate user.
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  constructor() {}
+}
